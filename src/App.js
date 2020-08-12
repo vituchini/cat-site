d0ce869fce676544e408a79ca3189ff8e0ebe848
Victor Petrovich <petrovichvictorpro99@gmail.com>
2020-08-12 14:02:37 +0300

    Second commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+import Header from "./components/Header.js";
+
+
+function App() {
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <Profile/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Profile() {
+    return (<div className='content'>
+        <div><img className='user-wallpaper' src="https://www.ubackground.com/_ph/17/10154132.jpg" alt=""/></div>
+        <div ><img className='waifu' src="https://waifu.clan.su/_ld/2/78541493.jpg" alt=""/></div>
+        <div>ava+opis</div>
+        <div>My posts
+            <div>New post</div>
+        </div>
+        <div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+        </div>
+    </div>)
+}
+
+export default Profile
